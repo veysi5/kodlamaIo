4714b3eced6dcbda7829602da75dc8eebe53e6c1
Veysi AKAN <veysiakan5@gmail.com>
2021-05-06 00:10:54 +0300

    5.GunDersKodları

---
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -0,0 +1,23 @@
+package nLayeredDemo;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
+import nLayeredDemo.dataAccess.concretes.AbcPrductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//ToDo: Spring IoC ile çözülecek
+		
+		ProductService productService = new ProductManager(new AbcPrductDao(),
+										new JLoggerManagerAdapter());
+		
+		Product product = new Product(1,2,"elma",12,50);
+		productService.add(product);
+
+	}
+
+}
